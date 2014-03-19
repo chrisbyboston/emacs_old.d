10fcaae8313d16baebbe97e8cfe8e85a765bfead
Chris Reynolds <chris@britecore.com>
2014-03-19 06:47:28 -0500

    Adding orgmode stuff

--- init.el
+++ init.el
@@ -1,5 +1,12 @@
+;; Add the package manager
 (when (>= emacs-major-version 24)
   (require 'package)
   (package-initialize)
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
   )
+
+;; Org-Mode stuff from http://orgmode.org/worg/org-tutorials/orgtutorial_dto.html
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
