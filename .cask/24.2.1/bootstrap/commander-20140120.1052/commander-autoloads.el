8116c3689579df49668e1e854cb5d496f42251a2
Chris Reynolds <chris@britecore.com>
2014-03-19 14:31:42 -0500

    Removing gitignored files and dirs

--- .cask/24.2.1/bootstrap/commander-20140120.1052/commander-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; commander-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("commander-pkg.el" "commander.el") (21288
-;;;;;;  54091 766771))
-
-;;;***
-
-(provide 'commander-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; commander-autoloads.el ends here
