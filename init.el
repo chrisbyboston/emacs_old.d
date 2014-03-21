326a6e202e2d1b9972d498cc733a4e2c57247681
Chris Reynolds <chris@britecore.com>
2014-03-20 21:21:51 -0500

    Theme and mouse-mode

--- init.el
+++ init.el
@@ -41,3 +41,21 @@
 
 ;; Implement vi-style dd
 (global-set-key (kbd "C-c d") 'kill-current-line)
+
+;; Set theme
+(custom-set-variables '(custom-enabled-themes (quote (tango-dark))))
+(custom-set-faces)
+
+;; Add two-finger scrolling
+(unless window-system
+  (require 'mouse)
+  (xterm-mouse-mode t)
+  (global-set-key [mouse-4] '(lambda ()
+                              (interactive)
+                              (scroll-down 1)))
+  (global-set-key [mouse-5] '(lambda ()
+                              (interactive)
+                              (scroll-up 1)))
+  (defun track-mouse (e))
+  (setq mouse-sel-mode t)
+)
